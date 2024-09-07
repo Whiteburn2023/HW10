ac1e70b91246c05a19241ad27ae4eea54a67093c
Whiteburn2023 <erunda995@yandex.ru>
2024-09-07 20:40:53 +0500

    ready

--- Task 1/src/Main.java
+++ Task 1/src/Main.java
@@ -8,8 +8,10 @@ public class Main {
         while (true) {
             String name = inputBookName();
             int pageCount = inputPageCount();
+            double price = inputPrice();
             addBook(name);
             addBook(name, pageCount);
+            addBook(name, pageCount, price);
             //todo использовать новый метод тут
             printInfo();
         }
@@ -24,14 +26,24 @@ public class Main {
         System.out.println("Введите кол-во страниц:");
         return new Scanner(System.in).nextInt();
     }
+    public static double inputPrice() {
+        System.out.println("Введите цену:");
+        return new Scanner(System.in).nextDouble();
+    }
+
 
     public static void addBook(String bookName) {
-        addBook(bookName, 0);
+        addBook(bookName, 0, 0.0);
     }
 
     public static void addBook(String bookName, int pageCount) {
         booksInfo = booksInfo + bookName + " - " + (pageCount > 0 ? pageCount : "N/A") + " стр.\n";
     }
+
+    public static void addBook(String bookName, int pageCount, double price) {
+        booksInfo = booksInfo + bookName + " - " + (pageCount > 0 ? pageCount : "N/A") + " стр. ; " + (price > 0 ? price : "N/A") + " - " + " руб.\n";
+    }
+
 
     public static void printInfo() {
         System.out.println(booksInfo);
